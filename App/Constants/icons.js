2e4df1a5804d14db2a73afccfaebedc8b07e51ca
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2022-01-22 12:37:53 +0530

    Initial Commit

---
+++ App/Constants/icons.js
@@ -0,0 +1,9 @@
+const CatHead = require('../src/assets/icons/cathead.png')
+const Heart = require('../src/assets/icons/heart.png')
+const Cat = require('../src/assets/icons/Cat.png')
+
+export default {
+  CatHead,
+  Cat,
+  Heart,
+}
